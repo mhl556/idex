d2f267a7fa6ef46e96853fb8247a2babcfec79ae
jduhd <3498727358@qq.com>
2025-05-05 18:10:41 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+import PracticeCard from '@/components/PracticeCard';
+export default function HomePage() {
+  return (
+    <div>
+      <h1>我的主页</h1>
+      <p>欢迎来到我的Next.js应用。</p>
+      <PracticeCard />
+      <PracticeCard />
+      <PracticeCard />
+    </div>
+  );
+}
