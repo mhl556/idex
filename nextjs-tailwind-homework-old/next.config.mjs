95f853b2b2e03f8b358d46df3002ed6ae54a9f8d
mhl556 <3498727358@qq.com>
2025-06-29 14:09:17 +0800

    fix: Attempt to remove non-existent .gitmodules

--- nextjs-tailwind-homework-old/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@whatwg-node/fetch'],
-  },
-};
-
-export default nextConfig;
